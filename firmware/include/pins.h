c34b7c55d84278c1c2692d6cd6702d8a7a25c319
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-20 15:46:13 +0300

    Refactor pins.h to remove unused definitions and improve organization

--- firmware/include/pins.h
+++ firmware/include/pins.h
@@ -1,22 +1,15 @@
-#pragma once
+#ifndef PINS_H
+#define PINS_H
 
-// Sensors
-#define ONE_WIRE_BUS 4
-#define DHT_PIN 15
+#define ONE_WIRE_BUS     4
 
-// Relays
-#define HEATER_RELAY 16
-#define FAN_RELAY 17
+#define HEATER_RELAY    16
+#define FAN_RELAY       17
 #define HUMIDIFIER_RELAY 18
-#define TURNER_RELAY 19
+#define TURNER_RELAY    19
 
-// Indicators
-#define BUZZER_PIN 23
-#define ALARM_LED 2
-#define WIFI_LED 5
+#define BUZZER_PIN      23
+#define ALARM_LED       22
+#define WIFI_LED        21
 
-// Buttons (optional)
-#define BTN_MENU 32
-#define BTN_UP   33
-#define BTN_DOWN 25
-#define BTN_OK   26
+#endif

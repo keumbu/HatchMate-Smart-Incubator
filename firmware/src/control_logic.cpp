72071e0214727753fb2a8fd4a923a9421185027b
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-20 16:05:20 +0300

    Refactor control_logic.cpp to simplify control functions and remove unused variables

--- firmware/src/control_logic.cpp
+++ firmware/src/control_logic.cpp
@@ -1,67 +1,39 @@
+#include <Arduino.h>
 #include "control_logic.h"
 #include "sensors.h"
 #include "actuators.h"
 #include "config.h"
-#include "display.h"
-#include "utils.h"
-#include <Arduino.h>
 
-// Timing variables
-unsigned long lastEggTurn = 0;
-unsigned long dayStartMillis = 0;
-int dayCounter = 1;
+static unsigned long lastTurn = 0;
+static uint8_t day = 1;
 
-// Hatching mode flag
-bool hatchingMode = false;
-
-void updateDayCounter() {
-    // Each day = 24 hours for simulation you can adjust
-    if(millis() - dayStartMillis > 86400000) { // 24*60*60*1000 ms
-        dayCounter++;
-        dayStartMillis = millis();
-    }
+void controlInit() {
+    lastTurn = millis();
 }
 
-void controlTemperature() {
-    float temp = getTemperature();
-    if(temp < TEMP_TARGET - TEMP_TOLERANCE) {
-        heaterOn();
-        fanOff();
-    } else if(temp > TEMP_TARGET + TEMP_TOLERANCE) {
-        heaterOff();
-        fanOn();
+void controlUpdate() {
+    float t = getTemperature();
+    float h = getHumidity();
+
+    if (t < TEMP_TARGET - TEMP_TOLERANCE) {
+        heaterOn(); fanOff();
+    } else if (t > TEMP_TARGET + TEMP_TOLERANCE) {
+        heaterOff(); fanOn();
     }
-}
 
-void controlHumidity() {
-    float hum = getHumidity();
-    float targetHum = (dayCounter <= 18) ? HUMIDITY_DAY_1_18 : HUMIDITY_DAY_19_21;
+    if (day <= 18) {
+        if (h < HUMIDITY_DAY_1_18) humidifierOn();
+        else humidifierOff();
 
-    if(hum < targetHum) {
-        humidifierOn();
-    } else {
-        humidifierOff();
+        if (millis() - lastTurn >= EGG_TURN_INTERVAL) {
+            turnerOn();
+            delay(3000);
+            turnerOff();
+            lastTurn = millis();
+        }
     }
-}
-
-void handleEggTurning() {
-    if(hatchingMode) return; // Do not turn eggs in hatching mode
-
-    if(millis() - lastEggTurn > EGG_TURN_INTERVAL) {
-        turnerOn();
-        delay(5000); // 5s turn
+    else {
+        humidifierOn();  // hatching mode
         turnerOff();
-        lastEggTurn = millis();
-    }
-}
-
-void checkHatchingMode() {
-    if(dayCounter >= 21 && !hatchingMode) {
-        hatchingMode = true;
-        turnerOff();
-        heaterOff();
-        humidifierOn(); // maintain 70% humidity
-        showAlertScreen("HATCHING MODE");
-        beepShort(); beepShort(); beepShort();
     }
 }

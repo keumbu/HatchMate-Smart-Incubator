59910993fda2aa7e54597801b304400f58792154
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-20 16:08:46 +0300

    Refactor main.cpp to streamline setup and loop functions, removing unused initializations and updates

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,31 +2,20 @@
 #include "sensors.h"
 #include "actuators.h"
 #include "display.h"
-#include "wifi_manager.h"
 #include "alerts.h"
 #include "control_logic.h"
-#include "utils.h"
 
 void setup() {
     Serial.begin(115200);
-
-    initActuators();
-    initSensors();
-    initDisplay();
-    initWiFi();
-
-    showStartupScreen();
-    beepShort();
+    actuatorsInit();
+    sensorsInit();
+    displayInit();
+    controlInit();
 }
 
 void loop() {
-    readSensors();
-    updateDayCounter();
-    controlTemperature();
-    controlHumidity();
-    handleEggTurning();
-    checkHatchingMode();
-    checkAlerts();
-    updateDisplay();
-    delay(500);
+    sensorsUpdate();
+    controlUpdate();
+    alertsUpdate();
+    displayUpdate();
 }

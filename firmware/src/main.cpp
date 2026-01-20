b9c317ff0ee364cdef0585198cc0fb941eacb3bb
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-20 15:36:27 +0300

    Refactor main.cpp to organize includes and streamline setup and loop functions

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "sensors.h"
+#include "actuators.h"
+#include "display.h"
+#include "wifi_manager.h"
+#include "alerts.h"
+#include "control_logic.h"
+#include "utils.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+
+    initActuators();
+    initSensors();
+    initDisplay();
+    initWiFi();
+
+    showStartupScreen();
+    beepShort();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    readSensors();
+    updateDayCounter();
+    controlTemperature();
+    controlHumidity();
+    handleEggTurning();
+    checkHatchingMode();
+    checkAlerts();
+    updateDisplay();
+    delay(500);
 }

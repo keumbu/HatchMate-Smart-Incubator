62acdb614b11c5b457d37c86b96bff0676a1db39
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-20 20:50:45 +0300

    Update sensors module implementation and interfaces

---
+++ firmware/include/sensors.h
@@ -0,0 +1,10 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+
+void initSensors();
+void readSensors();
+
+float getTemperature();
+float getHumidity();
+
+#endif
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "sensors.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    initSensors();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    readSensors();
+    Serial.print("Temp: ");
+    Serial.print(getTemperature());
+    Serial.print(" °C | Humidity: ");
+    Serial.println(getHumidity());
+    delay(2000);
 }

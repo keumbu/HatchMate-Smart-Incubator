30875a9c16db12e53abcd0f899246bfee48712ca
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-20 16:02:59 +0300

    Refactor alerts.cpp to streamline alert handling and improve clarity

--- firmware/src/alerts.cpp
+++ firmware/src/alerts.cpp
@@ -3,44 +3,36 @@
 #include "sensors.h"
 #include "actuators.h"
 #include "display.h"
-#include "utils.h"
 #include "config.h"
-#include "pins.h"
-#include "wifi_manager.h"
 
-bool alert_state = false;
+static bool alertActiveFlag = false;
 
-bool alertActive() { return alert_state; }
+void alertsUpdate() {
+    float t = getTemperature();
+    float h = getHumidity();
 
-void checkAlerts() {
-    float temp = getTemperature();
-    float hum = getHumidity();
-
-    if(temp > TEMP_HIGH_LIMIT) {
-        alert_state = true;
+    if (t > TEMP_HIGH_LIMIT) {
+        alertActiveFlag = true;
         heaterOff();
         fanOn();
-        digitalWrite(ALARM_LED,HIGH);
-        showAlertScreen("HIGH TEMP");
-        beepContinuous();
-    } else if(temp < TEMP_LOW_LIMIT) {
-        alert_state = true;
-        digitalWrite(ALARM_LED,HIGH);
-        showAlertScreen("LOW TEMP");
-        beepShort();
-    } else if(hum < HUMIDITY_DAY_1_18) {
-        alert_state = true;
-        digitalWrite(ALARM_LED,HIGH);
-        showAlertScreen("LOW HUMIDITY");
-        beepShort();
-    } else if(!isWiFiConnected()) {
-        alert_state = true;
-        digitalWrite(WIFI_LED, HIGH); // fast blink placeholder
-        showAlertScreen("Wi-Fi Lost");
-        beepShort(); beepShort();
-    } else {
-        alert_state = false;
+        digitalWrite(ALARM_LED, HIGH);
+        displayShowAlert("HIGH TEMP");
+    }
+    else if (t < TEMP_LOW_LIMIT) {
+        alertActiveFlag = true;
+        displayShowAlert("LOW TEMP");
+    }
+    else if (h < HUMIDITY_LOW_LIMIT) {
+        alertActiveFlag = true;
+        displayShowAlert("LOW HUMIDITY");
+    }
+    else {
+        alertActiveFlag = false;
         digitalWrite(ALARM_LED, LOW);
-        digitalWrite(WIFI_LED, HIGH);
     }
 }
+
+bool isAlertActive() {
+    return alertActiveFlag;
+}
+

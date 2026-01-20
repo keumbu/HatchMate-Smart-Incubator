0d44c8869c47d38be762b7391492845cecce1acc
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-20 13:15:01 +0300

    Add actuator control functions for heater, fan, humidifier, and turner

---
+++ actuators.h
@@ -0,0 +1,15 @@
+#pragma once
+
+void initActuators();
+
+void heaterOn();
+void heaterOff();
+
+void fanOn();
+void fanOff();
+
+void humidifierOn();
+void humidifierOff();
+
+void turnerOn();
+void turnerOff();

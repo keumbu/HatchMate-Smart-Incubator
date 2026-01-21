ad746debde7d4315040a44a2c40254ba2f5729c5
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-21 17:56:57 +0300

    Update entire sensors.h with new function declarations and stracture

--- include/sensors.h
+++ include/sensors.h
@@ -1,9 +1,19 @@
 #ifndef SENSORS_H
 #define SENSORS_H
 
+#include <Arduino.h>
+
+// Initialize sensors
 void initSensors();
+
+// Read all sensor values
 void readSensors();
+
+// Get latest temperature
 float getTemperature();
+
+// Get latest humidity
 float getHumidity();
 
 #endif
+

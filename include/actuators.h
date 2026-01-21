658f01f28ec680307db7cf91b006c5130bae737f
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-21 17:14:29 +0300

    Add actuator function declarations and constants in actuators.h

---
+++ include/actuators.h
@@ -0,0 +1,10 @@
+#ifndef ACTUATORS_H
+#define ACTUATORS_H
+
+void initActuators();
+void heaterOn(); void heaterOff();
+void fanOn(); void fanOff();
+void humidifierOn(); void humidifierOff();
+void turnerOn(); void turnerOff();
+
+#endif

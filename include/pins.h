10b673bb235874e1f049a49e026e1526c35e9a06
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-21 14:54:52 +0300

    Add ESP32 pin mappings for HatchMate hardware

---
+++ include/pins.h
@@ -0,0 +1,34 @@
+#ifndef PINS_H
+#define PINS_H
+
+// ================= PINS =================
+
+// Relays
+#define HEATER_RELAY    12
+#define FAN_RELAY       13
+#define HUMIDIFIER_RELAY 14
+#define TURNER_RELAY    15
+
+// LEDs
+#define POWER_LED       2
+#define HEATER_LED      16
+#define FAN_LED         17
+#define HUMIDIFIER_LED  18
+#define TURNER_LED      19
+#define ALARM_LED       21
+#define WIFI_LED        22
+
+// Buzzer
+#define BUZZER_PIN      23
+
+// Sensors
+#define ONE_WIRE_BUS    4  // DS18B20
+// DHT22 or other sensors can be assigned pins here
+
+// Buttons  
+#define BTN_MENU        25
+#define BTN_UP          26
+#define BTN_DOWN        27
+#define BTN_ENTER       32
+
+#endif

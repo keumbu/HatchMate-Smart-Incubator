113da94db3d29588138a1e3638c8e921a8525cc1
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-22 13:35:28 +0300

    Rewrite utils.cpp implementation

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,18 +1,32 @@
 #include "utils.h"
 #include "pins.h"
-#include <Arduino.h>
 
-void beepShort() { digitalWrite(BUZZER_PIN,HIGH); delay(100); digitalWrite(BUZZER_PIN,LOW); }
-void beepContinuous() { digitalWrite(BUZZER_PIN,HIGH); }
-void beepMultiple(uint8_t times, uint16_t duration){
-    for(uint8_t i=0;i<times;i++){
-        digitalWrite(BUZZER_PIN,HIGH); delay(duration);
-        digitalWrite(BUZZER_PIN,LOW); delay(duration);
+// ================= BUZZER =================
+void beepShort() {
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(100);
+    digitalWrite(BUZZER_PIN, LOW);
+}
+
+void beepContinuous() {
+    digitalWrite(BUZZER_PIN, HIGH);
+}
+
+void beepMultiple(uint8_t times, uint16_t duration) {
+    for (uint8_t i = 0; i < times; i++) {
+        digitalWrite(BUZZER_PIN, HIGH);
+        delay(duration);
+        digitalWrite(BUZZER_PIN, LOW);
+        delay(duration);
     }
 }
-String secondsToHHMM(unsigned long seconds){
-    unsigned long h = seconds/3600;
-    unsigned long m = (seconds%3600)/60;
-    char buf[6]; sprintf(buf,"%02lu:%02lu",h,m);
-    return String(buf);
+
+// ================= TIME HELPERS =================
+String secondsToHHMM(unsigned long seconds) {
+    unsigned long hours = seconds / 3600;
+    unsigned long minutes = (seconds % 3600) / 60;
+
+    char buffer[6];
+    sprintf(buffer, "%02lu:%02lu", hours, minutes);
+    return String(buffer);
 }

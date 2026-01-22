b310eba5018ceb2893f594fe36f49c517757273a
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-22 13:27:21 +0300

    Rewrite main.cppnto connect all modules

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,28 @@
-#include <Arduino.h>
-#include "sensors.h"
-#include "actuators.h"
-#include "display.h"
 #include "wifi_manager.h"
-#include "alerts.h"
-#include "control_logic.h"
-#include "utils.h"
 
-int CURRENT_DAY = 7; 
-unsigned long lastTurnTime = 0;
-
-void setup(){
+void setup() {
     Serial.begin(115200);
+    initWiFi();   // Initialize Wi-Fi
     initActuators();
     initSensors();
     initDisplay();
-    initWiFi();
     showStartupScreen();
     beepShort();
 }
 
-void loop(){
+void loop() {
     readSensors();
+    
     controlTemperature(getTemperature());
-    controlHumidity(getHumidity(), CURRENT_DAY);
-    handleEggTurning(lastTurnTime, CURRENT_DAY);
+    controlHumidity(getHumidity());
+    handleEggTurning(millis(), lastTurnTime);
+
     checkAlerts(getTemperature(), getHumidity(), isWiFiConnected());
-    updateDisplay();
+
+    // Reconnect Wi-Fi if lost
+    if (!isWiFiConnected()) {
+        reconnectWiFi();
+    }
+
+    updateDisplay(); // Show Wi-Fi status on OLED
 }

70023d98de9109189a42e3e717c7718dfa6b4335
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-22 11:27:17 +0300

    Redesign alerts header interface

--- include/alerts.h
+++ include/alerts.h
@@ -1,7 +1,25 @@
 #ifndef ALERTS_H
 #define ALERTS_H
 
-void checkAlerts(float temperature,float humidity,bool wifiStatus);
+#include <Arduino.h>
+
+// ================= ALERT FUNCTIONS =================
+
+// Initialize alert system (if needed)
+void initAlerts();
+
+// Check all alerts: temperature, humidity, Wi-Fi
+void checkAlerts(float currentTemp, float currentHumid, bool wifiConnected);
+
+// Alert triggers
+void highTemperatureAlert();
+void lowTemperatureAlert();
+void lowHumidityAlert();
+void wifiFailureAlert();
+
+// Reset alerts
+void resetAlerts();
 
 #endif
+
 

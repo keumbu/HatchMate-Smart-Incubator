354564ec8b2538ada3786e3d16597ec3752b4e11
Ezra Keumbu Asiago <keumbuasiago@gmail.com>
2026-01-22 08:52:33 +0300

    Reorganize actuator definitions and function prototypes

--- include/actuators.h
+++ include/actuators.h
@@ -1,10 +1,32 @@
 #ifndef ACTUATORS_H
 #define ACTUATORS_H
 
+#include <Arduino.h>
+
+// ================= ACTUATOR INITIALIZATION =================
 void initActuators();
-void heaterOn(); void heaterOff();
-void fanOn(); void fanOff();
-void humidifierOn(); void humidifierOff();
-void turnerOn(); void turnerOff();
+
+// ================= HEATER =================
+void heaterOn();
+void heaterOff();
+
+// ================= FAN =================
+void fanOn();
+void fanOff();
+
+// ================= HUMIDIFIER =================
+void humidifierOn();
+void humidifierOff();
+
+// ================= EGG TURNER =================
+void turnerOn();
+void turnerOff();
+
+// ================= BUZZER =================
+void beepShort();
+void beepContinuous();
+
+// ================= LEDS =================
+void setAlarmLED(bool state);
 
 #endif
